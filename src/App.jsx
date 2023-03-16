7cd6df0f77f91e35143a40ffd93fb01767306e8f
Harshit Kumar <harshitclub@gmail.com>
2023-03-16 17:40:47 +0530

    chalnaari

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+import chalnaari from "./assets/chalnaari.svg";
+import chalnaariFace from "./assets/chalnaariFace.svg";
+
+function App() {
+  return (
+    <>
+      <nav>
+        <img src={chalnaariFace} />
+      </nav>
+      <div className="chalnaari">
+        <div className="comingSoon">
+          <img src={chalnaari} />
+          <h3>Coming Soon...</h3>
+          <h4>15 | 08 | 2023</h4>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default App;
